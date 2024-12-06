fa012222571f8ced3498968e37a25966cc297f01
rnishiza771 <rnishiza@bbs.co.jp>
2024-12-06 12:41:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const welcomeMessage = document.querySelector("h1");
     welcomeMessage.addEventListener("click", () => {
         welcomeMessage.style.color = "blue";
-        alert("Welcome to my interactive site!");
+        alert("やっほー、クリックしてくれてありがとう！");
     });
 });
